9a61b3d921231537b43baf76dec9227cf7ff71c8
Benceeeeeeee <144451434+Benceeeeeeee@users.noreply.github.com>
2025-03-04 15:04:12 +0100

    resource

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+async function main() {
+
+    for(let i = 0; i < 15; i++){
+        await prisma.rentals.create({
+            data:{
+                start_date: faker.date.recent(),
+                end_date: faker.date.soon(),
+                book:{
+                    connect:{
+                        id: faker.number.int({min: 1, max: 50})
+                    }
+                }
+            }
+        })
+    }
+
+
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
